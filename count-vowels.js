1450edce204d15c1b353cf205520f0e0a1e36a89
Julia <20698409+codepluspixels@users.noreply.github.com>
2026-06-09 00:29:15 -0500

    Added positive, negative and edge cases tests

--- count-vowels.js
+++ count-vowels.js
@@ -12,4 +12,3 @@ function countVowels(str) {
 }
 
 module.exports = countVowels;
-
--- tests/count-vowels.test.js
+++ tests/count-vowels.test.js
@@ -1,9 +1,20 @@
-const countVowels = require("../count-vowels.js");
+const countVowels = require("../count-vowels");
 
 describe("countVowels", () => {
 
+  // Positive case test (valid input with vowels)
   test("counts vowels in a word", () => {
     expect(countVowels("Eltoids")).toBe(3);
+  });
+
+  // Negative case test (valid input, but no vowels)
+  test("returns 0 when there are no vowels", () => {
+    expect(countVowels("shhhhh")).toBe(0);
+  });
+
+  // Edge case test (empty input)
+  test("returns 0 for empty string", () => {
+    expect(countVowels("")).toBe(0);
   });
 
 });
